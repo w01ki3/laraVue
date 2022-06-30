0c1f94ebe81534b939bc5debf5f74d9634e86513
w01ki3 <w01ki3@gmail.com>
2022-06-30 10:34:35 +0300

    Link-1 component created

---
+++ resources/js/components/Link1Component.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-3">
+                <div class="card">
+                    <div class="card-header">Link-1 Component</div>
+
+                    <div class="card-body">
+                        <h1>Welcome to link 1 component</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+    }
+</script>
