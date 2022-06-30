29c909c45ebf632a9a5ec2ad2f26e12af36eecae
w01ki3 <w01ki3@gmail.com>
2022-06-30 10:34:06 +0300

    home component created

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="row justify-content-center">
+            <div class="col-md-8 mt-3">
+                <div class="card">
+                    <div class="card-header">Home Component</div>
+
+                    <div class="card-body">
+                        <h1>Welcome to home component</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+    }
+</script>
