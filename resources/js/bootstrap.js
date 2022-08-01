6ad288b5e007024e5ae378ece087b29a2ca13ab6
w01ki3 <w01ki3@gmail.com>
2022-08-01 15:25:56 +0300

    for vite syntax {require | import}

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,5 @@
-window._ = require('lodash');
-
-try {
-    require('bootstrap');
-} catch (e) {}
+import _ from 'lodash';
+window._ = _;
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -10,7 +7,8 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+import axios from 'axios';
+window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
