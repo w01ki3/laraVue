517c7fc574e49561c24a2cd8b0d8eb3c95831e53
w01ki3 <w01ki3@gmail.com>
2022-08-01 22:21:01 +0300

    for bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -9,4 +10,9 @@ export default defineConfig({
         ]),
         vue()
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
