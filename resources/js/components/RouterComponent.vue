c4278006aa491b1361f4bbc4db2dc6d6ca26d738
w01ki3 <w01ki3@gmail.com>
2022-08-10 09:19:03 +0300

    public icon | fix

--- resources/js/components/RouterComponent.vue
+++ resources/js/components/RouterComponent.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="@/../img/vkIcon.png" height="50" alt="logo" />
-        </a>
+        <router-link to="/" class="navbar-brand" href="/">
+          <img src="img/vkIcon.png" height="50" alt="logo" />
+        </router-link>
         <button
           class="navbar-toggler"
           type="button"
